e506c1be4d9b5ec82557eb8f871d27ba0200e423
roadkill0 <kwierzbicki2024@elam.co.uk>
2024-11-12 09:27:55 +0000

    cool

---
+++ Assets/scripts/playermovement.cs
@@ -0,0 +1,46 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Eenmy Head"))
+        {
+            Destroy(collision.transform.parent.gameObject);
+        }
+    }
+
+  
+    bool IsGrounded()
+    {
+       return  Physics.CheckSphere(groundCheck.position, .1f, ground);
+    }
+
+}
